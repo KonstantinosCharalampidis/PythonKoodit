df2823e53a05b2a15c97b4566270eb53ada6f151
KonstantinosCharalampidis <kostasxaral01@gmail.com>
2025-08-22 15:25:41 +0300

    Class Problem

---
+++ Score.py
@@ -0,0 +1,8 @@
+a = int(input("Programming Score: "))
+b = int(input("Physics Score: "))
+c = int(input("Mathematics Score: "))
+sum = a + b + c
+Percentage = int((sum/300)*100)
+print (Percentage)
+
+
