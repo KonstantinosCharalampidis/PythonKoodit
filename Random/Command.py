dea298de125585b1208d7f8320795c68268792e6
KonstantinosCharalampidis <kostasxaral01@gmail.com>
2025-08-29 14:47:56 +0300

    mod 03

---
+++ Random/Command.py
@@ -0,0 +1,65 @@
+"""command = input("Enter command: ")
+while command !="stop":
+    print("We are free,what to do,let's " + command)
+    command = input("Enter command: ")
+print("Execution stopped explicitly")""" #command example
+
+"""---------------------------------------------------------------------"""
+
+"""import random
+comb = input("Choose combination code: ")
+
+if comb == "three digit":
+    a = random.randint(1,10)
+    random.randint(3 * 3)
+    print(a)"""
+
+"""---------------------------------------------------------------------"""
+
+"""import random
+dice1 = dice2 =rolls = 0
+while(dice1 !=6 or dice2 != 6):
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    rolls = rolls + 1
+print(f"Rolled {rolls:d} times,")"""
+
+"""---------------------------------------------------------------------"""
+
+"""first = 1
+while first <= 5:
+    second = 1
+    while second <= 5:
+        print(f"{first} times {second} is {first*second:d}")
+        second = second + 1
+    first = first +1  """
+
+"""---------------------------------------------------------------------"""
+
+"""first = 2
+while first <= 2:
+    second = 1
+    while second <= 5:
+        print(f"{first} times {second} is {first*second:d}")
+        second = second + 1
+    first = first +2 """
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
