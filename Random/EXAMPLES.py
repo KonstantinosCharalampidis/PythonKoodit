0a9b8236ab6ab68edde02d67280af8dafa3c9eae
konstantinos <kostasxaral01@gmail.com>
2025-09-24 21:06:04 +0300

    e

---
+++ Random/EXAMPLES.py
@@ -0,0 +1,18 @@
+"""
+x=9
+print(f"The price is {x:.2f} euros.")
+
+
+mylist=["apple","orange","watermelon"]
+i = 0
+while i<len(mylist):
+    print(mylist[i])
+    i+=1"""
+
+i = 1
+while i < 6:
+    i+=1
+    if i == 3:
+        continue
+    print(i)
+
